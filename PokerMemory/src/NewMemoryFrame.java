025da6847491495837ffa94235a5a15fce8089a7
mcnovoa <maria.novoa1@upr.edu>
2017-11-27 18:35:24 -0400

    Added returnRankValue method

--- PokerMemory/src/NewMemoryFrame.java
+++ PokerMemory/src/NewMemoryFrame.java
@@ -1,30 +1,78 @@
-import java.awt.Component;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class NewMemoryFrame extends MemoryFrame {
-
-	private static final boolean DEBUG = true;
-	private JPanel contentPane;
-	private TurnsTakenCounterLabel turnCounterLabel;
-	private GameLevel difficulty;
-	private JPanel centerGrid;
-	private JLabel levelDescriptionLabel;
-	private ScoreManagement score;
-
-	public NewMemoryFrame() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-
-
+	ScoreManagement score = new ScoreManagement();
 	
-	
+    public NewMemoryFrame() {
+        super();
+        super.setScore(score.getScore());
+        JMenuBar menuBar = this.getJMenuBar();
+        JMenu memoryMenu = null;
+        for (int i = 0; i < menuBar.getMenuCount(); i++) {
+            if (menuBar.getMenu(i).getText().equals("Memory")) {
+                memoryMenu = menuBar.getMenu(i);
+                break;
+            }
+        }
+        
+        ActionListener menuHandler = new ActionListener() {
+            public void actionPerformed1(ActionEvent e) {
+                try {
+                    if(e.getActionCommand().equals("Same Rank Trio Score")) newGame("ranktrioscore");
+                } catch (IOException e2) {
+                    e2.printStackTrace(); throw new RuntimeException("IO ERROR");
+                }
+            }
 
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+        };
 
-	
+        JMenuItem rankTrioScoreMenuItem = new JMenuItem("Same Rank Trio Score");
+        rankTrioScoreMenuItem.addActionListener(menuHandler);
+        memoryMenu.add(rankTrioScoreMenuItem);
+    }
+
+    /**
+     * Prepares a new game (first game or non-first game)
+     * @throws IOException 
+     */
+    public void newGame(String difficultyMode) throws IOException
+    {
+        // Reset the turn counter label
+        this.getTurnCounterLabel().reset();
+        
+        //Set Score
+        this.setScore(score.getScore());
+        
+        // make a new card field with cards, and add it to the window
+
+//        if(difficultyMode.equalsIgnoreCase("ranktrioscore")) {
+//            this.setGameLevel(new RankTrioWithScore(this.getTurnCounterLabel(), this));
+//            this.getLevelDescriptionLabel().setText("Same Rank Trio Level Score");
+//            this.getTurnCounterLabel().reset();
+//
+//            // clear out the content pane (removes turn counter label and card field)
+//            BorderLayout bl  = (BorderLayout) this.getContentPane().getLayout();
+//            this.getContentPane().remove(bl.getLayoutComponent(BorderLayout.CENTER));
+//            this.getContentPane().add(showCardDeck(), BorderLayout.CENTER);
+//
+//            // show the window (in case this is the first game)
+//            this.setVisible(true);
+//        }
+//        else {
+//            super.newGame(difficultyMode);
+//        }
+    }
+
 }
--- PokerMemory/src/ScoreManagement.java
+++ PokerMemory/src/ScoreManagement.java
@@ -2,7 +2,7 @@
  * Inherits from JLabel and implements the management of the score based on the Level
  *
  * @author María C. Novoa García (original author)
- * @author Alberto Canela Cubero (contribuitor)
+ * @author Alberto Canela Cubero ()
  * @version Nov 2017
  */
 
@@ -19,15 +19,87 @@ public class ScoreManagement extends JLabel {
 	private EqualPairLevel equalPairLevel;
 	private RankTrioLevel rankTrioLevel;
 	private GameLevel gameLevel;
-	private ArrayList <Card> grid= new ArrayList<Card>();
-	private MemoryFrame mFrame = new MemoryFrame();
-	
-	
+	private int rankValue;
+
+	final private int JValue = 11;
+	final private int QValue = 12;
+	final private int KValue = 13;
+	final private int AValue = 20;
+	final private int tenValue = 10;
+
+
+	//	private ArrayList <Card> grid= new ArrayList<Card>();
+
+
 	public ScoreManagement()
 	{
 		super();
 		reset();
 	}
+
+	public int returnRankValue(Card card){
+
+		if(card.getRank().equals("t")){
+			rankValue = tenValue;
+		}
+		else if(card.getRank().equals("j")){
+			rankValue = JValue;
+		}
+		else if(card.getRank().equals("q")){
+			rankValue = QValue;
+		}
+		else if(card.getRank().equals("k")){
+			rankValue = KValue;
+		}
+		else if(card.getRank().equals("a")){
+			rankValue = AValue;
+		}
+
+
+		return rankValue;
+	}
+	/**
+	 * Returns the Score according to it's level mode
+	 * 
+	 * @param card type Card representing the first card on the buffer
+	 * @param card1 type Card representing the first card on the buffer
+	 * @param card2 type Card representing the first card on the buffer
+	 * 
+	 * @return score
+	 */
+
+	public long returnTheScore(Card card, Card card1, Card card2){
+		card = gameLevel.getTurnedCardsBuffer().get(0);
+		card1 = gameLevel.getTurnedCardsBuffer().get(1);
+		card2 = gameLevel.getTurnedCardsBuffer().get(2);
+
+		if(gameLevel.getMode().equals(equalPairLevel.getMode())){
+			//Score Instructions for "EqualPairLevel"
+			if( (card.getRank().equals(card1.getRank())) && (card.getSuit().equals(card1.getSuit()))){
+				score+= 50;
+				update();
+			}
+			else {
+				score =- 5;
+				update();}
+
+		}
+
+		else if(gameLevel.getMode().equals(rankTrioLevel.getMode())){
+			//Score Instructions for "RankTrioLevel"
+
+			if(card.getRank().equals(card1.getRank()) &&  card.getRank().equals(card2.getRank())
+					&& card.getSuit().equals(card1.getSuit()) &&  card.getSuit().equals(card2.getSuit())){
+				score =+ 100 + this.returnRankValue(card) + this.returnRankValue(card1) + this.returnRankValue(card2);
+				update();
+
+			}
+			else {score =- 5;}
+
+		}
+		return score;
+	}
+
 	public long getScore() {
 		return score;
 	}
@@ -39,40 +111,12 @@ public class ScoreManagement extends JLabel {
 	private void update()
 	{
 		this.setText("" + this.getScore());
-//		this.setHorizontalAlignment(SwingConstants.RIGHT);
 	}
 
 	public void reset()
 	{
 		this.score = 0;
 		update();
-	}
-
-	public long returnTheScore(){
-
-		if(gameLevel.getMode().equals(equalPairLevel.getMode())){
-			//Score Instructions for "EqualPairLevel"
-
-
-		}
-
-		else if(gameLevel.getMode().equals(rankTrioLevel.getMode())){
-			//Score Instructions for "RankTrioLevel"
-			Card card = gameLevel.getTurnedCardsBuffer().get(0);
-			Card card1 = gameLevel.getTurnedCardsBuffer().get(1);
-			Card card2 = gameLevel.getTurnedCardsBuffer().get(2);
-			if(card.getRank().equals(card1.getRank()) &&  card.getRank().equals(card2.getRank())){
-				score =+ 100 + Integer.parseInt(card.getRank()) + Integer.parseInt(card1.getRank())
-				+ Integer.parseInt(card2.getRank());
-				update();
-
-			}
-			//		if((rankTrioLevel.turnUp()
-			//				card.getRank().equals(otherCard1.getRank())) && (card.getRank().equals(otherCard2.getRank()))) {
-			//	}
-
-		}
-		return score;
 	}
 }
 
