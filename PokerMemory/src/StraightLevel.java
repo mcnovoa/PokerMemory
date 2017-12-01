cb7c9a6a638f75dc01318c465c55d1e24322fb28
mcnovoa <maria.novoa1@upr.edu>
2017-12-01 07:42:33 -0400

    Commented isGameOver that doesn't work

--- PokerMemory/src/StraightLevel.java
+++ PokerMemory/src/StraightLevel.java
@@ -1,16 +1,13 @@
 import java.util.Arrays;
-
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
 public class StraightLevel extends FlushLevel {
 
 	private long score;
-	private boolean areCombinationsLeft;
+	private GameLevel level;
 
 	protected StraightLevel(TurnsTakenCounterLabel validTurnTime, JFrame mainFrame) {
 		super(validTurnTime, mainFrame);
-		areCombinationsLeft = true;
 		// TODO Auto-generated constructor stub
 	}
 
@@ -25,37 +22,37 @@ public class StraightLevel extends FlushLevel {
 		return score;
 	}
 	
-	protected boolean isGameOver(Card a, Card b, Card c, Card d, Card e) {
-		
-		Card[] newArr = new Card[this.getGrid().size()];
-
-		for (int i = 0; i < newArr.length ; i++) {
-			if(!(this.getGrid().get(i).isFaceUp())){
-				a = this.getGrid().get(i);
-			}
-			else if (!(this.getGrid().get(i).isFaceUp()) && (this.getGrid().get(i) != a)){
-				b = this.getGrid().get(i);
-			}
-			else if(!(this.getGrid().get(i).isFaceUp()) && (this.getGrid().get(i) != a) && (this.getGrid().get(i) != b)){
-				c = this.getGrid().get(i);
-			}
-			else if(!(this.getGrid().get(i).isFaceUp()) && (this.getGrid().get(i) != a) 
-				&& (this.getGrid().get(i) != b) && (this.getGrid().get(i) != c)){
-				d = this.getGrid().get(i);
-			}
-			else if(!(this.getGrid().get(i).isFaceUp()) && (this.getGrid().get(i) != a) 
-					&& (this.getGrid().get(i) != b) && (this.getGrid().get(i) != c) 
-					&& (this.getGrid().get(i) != d)){
-				e = this.getGrid().get(i);
-			}
-		}
-
-		if(ComboLevel.isStraight(a, b, c, d, e, this)){
-			return false;
-		}
-
-		return true;
-	}
+//	protected boolean isGameOver(Card a, Card b, Card c, Card d, Card e) {
+//		
+//		Card[] newArr = new Card[this.getGrid().size()];
+//
+//		for (int i = 0; i < newArr.length ; i++) {
+//			if(!(this.getGrid().get(i).isFaceUp())){
+//				a = this.getGrid().get(i);
+//			}
+//			else if (!(this.getGrid().get(i).isFaceUp()) && (this.getGrid().get(i) != a)){
+//				b = this.getGrid().get(i);
+//			}
+//			else if(!(this.getGrid().get(i).isFaceUp()) && (this.getGrid().get(i) != a) && (this.getGrid().get(i) != b)){
+//				c = this.getGrid().get(i);
+//			}
+//			else if(!(this.getGrid().get(i).isFaceUp()) && (this.getGrid().get(i) != a) 
+//				&& (this.getGrid().get(i) != b) && (this.getGrid().get(i) != c)){
+//				d = this.getGrid().get(i);
+//			}
+//			else if(!(this.getGrid().get(i).isFaceUp()) && (this.getGrid().get(i) != a) 
+//					&& (this.getGrid().get(i) != b) && (this.getGrid().get(i) != c) 
+//					&& (this.getGrid().get(i) != d)){
+//				e = this.getGrid().get(i);
+//			}
+//		}
+//
+//		if(ComboLevel.isStraight(a, b, c, d, e, this)){
+//			return false;
+//		}
+//
+//		return true;
+//	}
 
 	@Override
 	protected boolean turnUp(Card card) {
@@ -155,51 +152,5 @@ public class StraightLevel extends FlushLevel {
 	{
 		score += 1000 + 100*highRank;
 		this.getMainFrame().setScore(score);
-		if(straightCombinationsLeft() == 0) {
-			areCombinationsLeft = false;
-		}
-	}
-	
-	//GameOver when there are no more straigt combinations. Show end Message.
-	@Override
-	protected boolean  isGameOver(){
-		if(!areCombinationsLeft){
-			//Show Ending Messages
-			String GameOver = "Congratulations you have reach the end of the game\r\n"+
-			"There is no more straight combinations left\r\n\r\n Your Score: "+score+"\r\nMoves Made: "+this.getTurnsTakenCounter().getText();
-			JOptionPane.showMessageDialog(this.getMainFrame(), GameOver, "Game Over", JOptionPane.PLAIN_MESSAGE);
-			return true;
-		}
-		return false;
-	}
-		
-	//Find the total number of valid combinations in the grid.
-	public int straightCombinationsLeft() {
-		boolean[] rankExistance = new boolean[13];
-		int combinations = 0;
-		//Get if there are cards facedown with a given rank
-		for (int i =0; i< this.getGrid().size();i++) {
-			if(!this.getGrid().get(i).isFaceUp()) {
-				if(this.getGrid().get(i).getRank().equals("a")){
-					rankExistance[0] = true;
-					}
-					else {
-						rankExistance[ScoreManagement.returnRankValue(this.getGrid().get(i))-1] = true;
-					}
-			}
-	    }
-		//Look for posible straight combinations (includes invalid flush straights)
-		for(int i=4;i<rankExistance.length;i++)
-		{
-			if((rankExistance[i-4])&&rankExistance[i-3]&&rankExistance[i-2]&&rankExistance[i-1]&&rankExistance[i]) {
-				combinations++;
-			}
-		}
-		//Special case where rank a is highest:
-		if(rankExistance[0]&&rankExistance[12]&&rankExistance[11]&&rankExistance[10]&&rankExistance[9]){
-			combinations++;
-		}
-		MemoryFrame.dprintln("combinations Result "+ combinations);
-		return combinations;
 	}
 }
